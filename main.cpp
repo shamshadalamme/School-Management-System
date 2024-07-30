3bdd0f3daf0d8e774de5c2ef3e506a216efda824
shamshadalamme <sashamshadalam977@gmail.com>
2024-07-31 00:42:58 +0200

    improvised main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main()
 {
-    cout << "------------Safa Public School----------\n"
+    cout << "------------Depot Secondary School----------\n"
          << "\t1- Add new Student\n"
          << "\t2- Add new Teacher\n"
          << "\t3- Show record of Students and Teachers with ID\n"
          << "\t4- Show ALL"
          << "----------------------------------------\n";
+    
+    int input;
+    cin >> input;
+
 
     return 0;
 }
